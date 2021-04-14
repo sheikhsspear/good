9194c10fee7401d3b6877bc1b96abf49929f931d
sheikhsspear <get.shaikimam@gmail.com>
2021-04-14 15:30:38 -0400

    initialization

--- good_hands/settings.py
+++ good_hands/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps/dashboard',
+    'apps/landing_page',
 ]
 
 MIDDLEWARE = [
